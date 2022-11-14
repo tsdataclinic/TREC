9028ac3e00dca7930adb7307101b0db718337205
CanyonFoot <canyon.foot@twosigma.com>
2022-11-14 16:22:55 +0000

    Make interpolation more flexible

--- analysis/src/features/areal_interpolation.py
+++ analysis/src/features/areal_interpolation.py
@@ -13,24 +13,24 @@ def calculate_areal_weights(polygons, census_geo, polygon_id_col):
     intersection_weights = intersected_polygons[[polygon_id_col, "GEOID", "intersection_weight"]]
     return intersection_weights
 
-def aggregate_values(census_data, areal_weights, polygon_id_col):
+def aggregate_values(census_data, areal_weights, polygon_id_col, drop_cols = ["NAME", "intersection_weight"]):
     
     census_data["GEOID"] = census_data["GEOID"].astype(str)
     areal_weights["GEOID"] = areal_weights["GEOID"].astype(str)
 
     census_areal = census_data.merge(areal_weights, how = "inner")
-    census_values = census_areal.drop(["GEOID", "NAME", "intersection_weight"], axis = 1)
+    census_values = census_areal.drop(["GEOID"] + drop_cols, axis = 1)
     census_values.iloc[:, census_values.columns != polygon_id_col] = census_values.iloc[:, census_values.columns != polygon_id_col].apply(pd.to_numeric, errors='coerce')
     sums = pd.concat([census_values.iloc[:, census_values.columns != polygon_id_col].multiply(census_areal["intersection_weight"], axis = "index"),census_values[polygon_id_col]], axis = 1).groupby(polygon_id_col).sum() 
     return sums
 
-def main(census_geo, census_data, polygons, polygon_id_col, crs = "EPSG:2263"):
+def main(census_geo, census_data, polygons, polygon_id_col, drop_cols = ["NAME", "intersection_weight"], crs = "EPSG:2263"):
     
     census_geo = census_geo.to_crs(crs)
     polygons = polygons.to_crs(crs)
     
     census_geo = calculate_census_areas(census_geo)
     areal_weights = calculate_areal_weights(polygons, census_geo, polygon_id_col)
-    sums = aggregate_values(census_data, areal_weights, polygon_id_col)
+    sums = aggregate_values(census_data, areal_weights, polygon_id_col, drop_cols)
     
     return sums
