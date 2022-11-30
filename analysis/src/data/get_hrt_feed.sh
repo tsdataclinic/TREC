7670687774c35c3a8b02a55784085dd156a606e7
Kaushik Mohan <kaushik.mohan@twosigma.com>
2022-11-30 23:37:23 +0000

    fixing path issues in saving data

--- analysis/src/data/get_hrt_feed.sh
+++ analysis/src/data/get_hrt_feed.sh
@@ -2,5 +2,5 @@
 
 ## Hampton Roads Transit Feed data
 
-wget -O ~/../data/transit_feeds_data/hrt_feeds/google_transit_hrt.zip https://gtfs.gohrt.com/gtfs/google_transit.zip
-unzip ~/../data/transit_feeds_data/hrt_feeds/google_transit_hrt.zip -d ~/../data/transit_feeds_data/hrt_feeds/hrt/
+wget -O /home/data/transit_feeds_data/hrt_feeds/google_transit_hrt.zip https://gtfs.gohrt.com/gtfs/google_transit.zip
+unzip /home/data/transit_feeds_data/hrt_feeds/google_transit_hrt.zip -d /home/data/transit_feeds_data/hrt_feeds/hrt/
