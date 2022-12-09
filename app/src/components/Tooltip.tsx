8ce557f1be70540cfbd5e15b4b939a62c9eca727
Indraneel Purohit <indraneel.purohit@twosigma.com>
2022-12-09 13:30:33 -0500

    Update Tooltip.tsx

--- app/src/components/Tooltip.tsx
+++ app/src/components/Tooltip.tsx
@@ -1,22 +1,33 @@
 const Tooltip = ({feature} : any) => { 
     const { id , properties } = feature;
     return (
-    <div id={`tooltip-${id}`}>
+    <div id={`tooltip-${id}`} className="w-96  block">
+        <span className="font-bold">{properties['stop_name']}</span>
         <table>
         <tbody>
-        {Object.keys(properties).map((key) => {
-            return <tr key={key}>
-                <td><b>{key}</b></td>
-                <td>
-                    {typeof properties[key] !== 'number' ?
-                        properties[key] : 
-                        Number.isInteger(properties[key]) ? 
-                            properties[key] : 
-                            Number.parseFloat(properties[key]).toFixed(2)
-                    }
+            <tr>
+                <td>Routes: </td>
+                {/* <td>{(properties['routes_serviced']).join(", ")}</td> */}
+            </tr>
+            <tr>
+                <td>Flood risk: </td>
+                {/* <td>{properties['risk_category'].map(() => ('x'))}</td> */}
+                <td>{properties['risk_category']}</td>
+            </tr>
+            <tr>
+                <td>Hospital Access: </td>
+                <td className="grid gap-1 grid-cols-3 grid-rows-1">
+                    {properties['access_to_hospital']}
                 </td>
             </tr>
-        })}
+            <tr>
+                <td>Jobs Access: </td>
+                <td className="w-full grid gap-2 grid-cols-3 grid-rows-1">
+                    {[...Array(3)].map((_, i) => (
+                        <span className={`${ i <= properties['jobs_cat'] ? `bg-cyan-600` : `bg-slate-300`}`}>x</span>
+                    ))}
+                </td>
+            </tr>
         </tbody>
         </table>
     </div>
