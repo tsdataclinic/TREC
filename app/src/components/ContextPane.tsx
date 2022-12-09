d0d1c7f06a2ad405273437db8202109c13acae9e
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2022-12-09 14:54:30 -0500

    Nest `select` inside `label` for accessibility

--- app/src/components/ContextPane.tsx
+++ app/src/components/ContextPane.tsx
@@ -1,23 +1,26 @@
-import { Layer } from '../App';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { Layer } from "../App";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 type Props = {
   availableProperties: Set<string>;
   setSelectedProperties: React.Dispatch<React.SetStateAction<string[]>>;
   selectedProperties: Array<string>;
   layers: Record<string, Layer>;
-  updateLayer: (layer: Layer) => void
-}
+  updateLayer: (layer: Layer) => void;
+};
 
 function ContextPane({
   layers,
   updateLayer,
   availableProperties,
   selectedProperties,
-  setSelectedProperties
-} : Props) : JSX.Element {
+  setSelectedProperties,
+}: Props): JSX.Element {
   return (
-    <div id="ContextPane" className="bg-white w-96 h-5/6 t-100 fixed z-50 left-5 rounded-md">
+    <div
+      id="ContextPane"
+      className="bg-white w-96 h-5/6 t-100 fixed z-50 left-5 rounded-md"
+    >
       {/* TODO - move map to centroid of selected area on select */}
       <select className="text-2xl">
         <option>New York City area</option>
@@ -26,54 +29,62 @@ function ContextPane({
       <hr />
       {/* <div className="text-lg">Select transit routes</div> */}
       {Object.values(layers).map((layer) => {
-        return <div>
-          <FontAwesomeIcon onClick={() => { 
-              updateLayer({
+        return (
+          <div>
+            <FontAwesomeIcon
+              onClick={() => {
+                updateLayer({
                   ...layer,
-                  isVisible: !layer.isVisible
-              })
-            }}
+                  isVisible: !layer.isVisible,
+                });
+              }}
               size="1x"
-              cursor={'pointer'}
+              cursor={"pointer"}
               icon={layer.isVisible ? faEye : faEyeSlash}
-              title={layer.isVisible ? 'Hide layer' : 'Show layer'}
+              title={layer.isVisible ? "Hide layer" : "Show layer"}
             />
-          {layer?.layerName}
-          
-        </div>
+            {layer?.layerName}
+          </div>
+        );
       })}
       <hr />
       <div>
-      <div>
-        <label>Field 1: </label>
-        <select
-          onChange={(e) => setSelectedProperties([e.target.value, selectedProperties[1]])}
-          placeholder={'Select a field...'}
-          defaultValue={selectedProperties[0]}>
-            <option></option>
-          {Array.from(availableProperties).map(p => (
-            <option
-              selected={p === selectedProperties[0]}>
-                {p}
-            </option>)
-          )}
-        </select>
-      </div>
-      <div>
-        <label>Field 2: </label>
-        <select
-          onChange={(e) => setSelectedProperties([selectedProperties[0], e.target.value])}
-          placeholder={'Select a field...'}
-          defaultValue={selectedProperties[1]}>
-            <option></option>
-          {Array.from(availableProperties).map(p => (
-            <option
-              selected={p === selectedProperties[1]}>
-                {p}
-            </option>)
-          )}
-        </select>
-      </div>
+        <div>
+          <label>
+            Field 1:
+            <select
+              className="ml-2"
+              onChange={(e) =>
+                setSelectedProperties([e.target.value, selectedProperties[1]])
+              }
+              placeholder={"Select a field..."}
+              defaultValue={selectedProperties[0]}
+            >
+              <option></option>
+              {Array.from(availableProperties).map((p) => (
+                <option selected={p === selectedProperties[0]}>{p}</option>
+              ))}
+            </select>
+          </label>
+        </div>
+        <div>
+          <label>
+            Field 2:
+            <select
+              className="ml-2"
+              onChange={(e) =>
+                setSelectedProperties([selectedProperties[0], e.target.value])
+              }
+              placeholder={"Select a field..."}
+              defaultValue={selectedProperties[1]}
+            >
+              <option></option>
+              {Array.from(availableProperties).map((p) => (
+                <option selected={p === selectedProperties[1]}>{p}</option>
+              ))}
+            </select>
+          </label>
+        </div>
       </div>
     </div>
   );
