38dbba9df15077e2a879a5010a2f1f095b30f9f3
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2022-12-12 17:19:02 -0500

    Added modals for methodology and about pages

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,11 +1,71 @@
 import * as React from "react";
 import { NavLink } from "react-router-dom";
+import Modal from "./ui/Modal";
+import Button from "./ui/Button";
 
 function HeaderLink(props: { to: string; children: React.ReactNode }) {
+  const { to, children } = props;
   return (
-    <NavLink to={props.to} className="hover:bg-slate-600 py-4 px-2">
-      {props.children}
+    <NavLink to={to} className="hover:bg-slate-600 py-4 px-4">
+      {children}
     </NavLink>
+  );
+}
+
+function ModalLink(props: {
+  children: string;
+  modalContents: React.ReactNode;
+}) {
+  const { children, modalContents } = props;
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  return (
+    <>
+      <Button
+        unstyled
+        className="hover:bg-slate-600 px-4"
+        onClick={() => setIsModalOpen(true)}
+      >
+        {children}
+      </Button>
+      {isModalOpen && (
+        <Modal
+          isOpen={isModalOpen}
+          onDismiss={() => setIsModalOpen(false)}
+          title={children}
+        >
+          {modalContents}
+        </Modal>
+      )}
+    </>
+  );
+}
+
+function AboutModalContents() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+  );
+}
+
+function MethodologyModalContents() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
   );
 }
 
@@ -16,9 +76,11 @@ function Header() {
         <img width="40px" alt="two-sigma-data-clinic-logo" src="/logo.png" />
         <h1>Census TOP</h1>
       </div>
-      <div className="flex space-x-4">
-        <HeaderLink to="/">Methodology</HeaderLink>
-        <HeaderLink to="/">About</HeaderLink>
+      <div className="flex">
+        <ModalLink modalContents={<MethodologyModalContents />}>
+          Methodology
+        </ModalLink>
+        <ModalLink modalContents={<AboutModalContents />}>About</ModalLink>
         <HeaderLink to="/">Feedback</HeaderLink>
       </div>
     </header>
---
+++ app/src/components/ui/Button/index.tsx
@@ -0,0 +1,57 @@
+import classNames from "classnames";
+import type { ReactNode, MouseEventHandler } from "react";
+
+type Props = {
+  children: ReactNode;
+  className?: string;
+  intent?: "primary" | "danger" | "default";
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  type?: "button" | "submit";
+
+  unstyled?: boolean;
+
+  /**
+   * Render the button in different default styles.
+   * - `full`: Render the button with 100% width in its container and sharp edges.
+   */
+  variant?: "normal" | "full";
+};
+
+export default function Button({
+  children,
+  className,
+  onClick,
+  type = "button",
+  variant = "normal",
+  intent = "default",
+  unstyled = false,
+}: Props): JSX.Element {
+  const buttonClassName = classNames(
+    className,
+    "focus-visible:outline-fuchsia-700",
+    unstyled
+      ? undefined
+      : {
+          "py-2 px-4 transition-colors duration-200": true,
+          // variant-dependent styles
+          "w-full": variant === "full",
+
+          // intent-dependent styles (only apply if our variant isn't "unstyled")
+          "bg-blue-500 hover:bg-blue-400 active:bg-blue-500 text-white":
+            intent === "primary",
+          "bg-red-500 hover:bg-red-400 active:bg-red-500 text-white":
+            intent === "danger",
+          "bg-gray-200 hover:bg-gray-300 active:bg-gray-200 text-gray-800 hover:text-gray-900":
+            intent === "default",
+        }
+  );
+
+  return (
+    // disable button-has-type lint because we're receiving the type
+    // correctly from the `type` prop
+    // eslint-disable-next-line react/button-has-type
+    <button type={type} className={buttonClassName} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
