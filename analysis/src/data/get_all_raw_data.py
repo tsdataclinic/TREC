9cccc373a5e7be80b332426396541cbc3347a170
CanyonFoot <canyon.foot@twosigma.com>
2023-02-21 17:35:53 +0000

    subprocess fix

--- analysis/src/data/get_all_raw_data.py
+++ analysis/src/data/get_all_raw_data.py
@@ -1,7 +1,7 @@
-from get_transit_data import get_transit_feeds
-from get_LODES import get_LODES
-from get_POI_data import get_poi_data
-from get_walk_graph import make_walk_graph
+from src.data.get_transit_data import get_transit_feeds
+from src.data.get_LODES import get_LODES
+from src.data.get_POI_data import get_poi_data
+from src.data.get_walk_graph import make_walk_graph
 # from get_cenus_data import get_census_geographies
 import subprocess
 import argparse
@@ -13,7 +13,7 @@ def main():
     parser = argparse.ArgumentParser("Get all raw data")
     parser.add_argument("--config", required=True)
     parser.add_argument("--city", required=True)
-    
+    path = os.getcwd()
     opts = parser.parse_args()
     
     with open(opts.config) as f:
@@ -24,8 +24,7 @@ def main():
     print("Getting Transit feeds") 
     get_transit_feeds(config, opts.city)
     print("Getting Census geographies") 
-    subprocess.run(["get_census_data.R",opts.config,opts.city])
-    # get_census_geographies(config, opts.city)
+    subprocess.run(["Rscript", f"{path}/src/data/get_census_data.R",opts.config,opts.city])
     print("Getting LODES data") 
     get_LODES(config, opts.city)
     print("Getting OSM data") 
