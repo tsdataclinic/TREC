223f389167f6918c867fa5ac406b921d02eb96d3
bewouk <ben.wong@twosigma.com>
2023-04-26 13:10:38 -0400

    New TREC About page

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -31,148 +31,23 @@ function ModalLink(props: {
   );
 }
 
-function AboutModalContents() {
-  return (
-    <>
-      <h1 className="text-xl">
-        TREC (Transit Resilience for Essential Commuting)
-      </h1>
-      <p>
-        In the fall of 2022, Data Clinic took part in{' '}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://opportunity.census.gov/"
-        >
-          The Opportunity Project
-        </a>
-        , a semi-annual sprint organized by the U.S. Census in partnership with
-        federal agencies to demonstrate the value of open data, as part of the{' '}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://opportunity.census.gov/sprints/"
-        >
-          Building Climate Change Resilience Through Public Transit
-        </a>{' '}
-        sprint sponsored by the U.S. Department of Transportation.
-      </p>
-      <p>
-        Across our many conversations with transit officials, researchers, and
-        community organizers from across the country about the climate-related
-        challenges and opportunities transportation systems face, a recurring
-        theme was the desire to enable a better understanding of climate's
-        intersectional impact on both transit and communities. In other words, a
-        flooded bus stop doesn't just mean that the bus and passengers can't
-        access the stop, but it may also impede access to a hospital or
-        community support, or to a large amount of essential jobs. How can we
-        share that insight more effectively?
-      </p>
-      <p>
-        In response, we built Transit Resilience for Essential Commuting (TREC),
-        an open source tool that allows users to efficiently assess the climate
-        risk for transit stations within the context of the access it provides
-        to vital services and regions. Initially focused on flooding, the most
-        prevalent climate event facing transit officials across the country, and
-        access to hospital and jobs, TREC allows users to explore our open
-        data-derived, station-specific risk and access ratings, and easily
-        filter those with the highest climate risk and highest importance for
-        access.
-      </p>
-      <p>
-        Our hope is that this human-centered and geospatial approach to the
-        intersectional impact of climate change on transit and communities will
-        give transit planners a more holistic picture to inform their
-        infrastructure improvement decision-making. Further, we hope that making
-        localized climate resilience tools like this open source, user-friendly,
-        and publicly available, will empower community organizations to advocate
-        for their underserved constituents.
-      </p>
-      <p>
-        The climate crisis we face requires collective intelligence and creative
-        problem solving, and democratizing access to these kinds of tools will
-        be crucial in making progress.
-      </p>
-      <br />
-      <p>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/tsdataclinic/trec"
-        >
-          TREC Github repo
-        </a>
-      </p>
-      <br />
+function ButtonLink(props: {
+   children: string;
+   url: string;
+}) {
+   const { children, url } = props;
 
-      <h1 className="text-xl">What is Data Clinic?</h1>
-      <p>
-        Data Clinic is the data and tech-for-good arm of Two Sigma, a financial
-        sciences company headquartered in NYC.
-      </p>
-      <p>
-        Since Data Clinic was founded in 2014, we have provided pro bono data
-        science and engineering support to mission-driven organizations around
-        the world via close partnerships that pair Two Sigma’s talent and way of
-        thinking with our partner’s rich content-area expertise.
-      </p>
-      <p>
-        To scale the solutions and insights we have gathered over the years, and
-        to contribute to the democratization of data, we also engage in the
-        development of open source tooling and data products.
-      </p>
-      <p>
-        To learn more visit{' '}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://dataclinic.twosigma.com"
-        >
-          dataclinic.twosigma.com
-        </a>{' '}
-        and connect with us via{' '}
-        <a href="mailto:dataclinic@twosigma.com">dataclinic@twosigma.com</a>
-      </p>
-      <br />
-
-      <h1 className="text-xl">How can I contribute?</h1>
-      <p>
-        We want TREC to support your needs. That means that we need a lot of
-        voices helping us shape the features we develop.
-      </p>
-      <p>
-        If you would like to suggest a feature or improvement, please either{' '}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/tsdataclinic/trec/issues"
-        >
-          open a ticket on GitHub
-        </a>{' '}
-        or reach out to us by{' '}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="mailto:dataclinic@twosigma.com"
-        >
-          email
-        </a>
-        .
-      </p>
-      <p>
-        If you want to contribute in a technical capacity, head over to our{' '}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/tsdataclinic/trec"
-        >
-          GitHub page
-        </a>{' '}
-        to open issues, suggest features, contribute pull requests, and find
-        beginner issues.
-      </p>
-    </>
-  );
+   return (
+      <Button
+          unstyled
+          className="hover:bg-slate-600 px-4 py-4"
+          onClick={(e) => {
+	    e.preventDefault();
+	    window.location.href=url;
+	   }}>
+         {children}
+      </Button>
+   )
 }
 
 function LegalContents() {
@@ -531,8 +406,9 @@ function Header(props: { isMobile: boolean; }) {
       {
         isDropdownMenuOpen ?
         <div>
+	  <ButtonLink url="/">Home</ButtonLink>
           <ModalLink modalContents={<MethodologyModalContents />}>Methodology</ModalLink>
-          <a href='/about'>About</a>
+          <ButtonLink url="/about">About</ButtonLink>
           <ModalLink modalContents={<LegalContents />}>Legal</ModalLink>
         </div>
         :
@@ -543,8 +419,9 @@ function Header(props: { isMobile: boolean; }) {
       }
       <div className="flex">
         <div className='hidden sm:block'>
+	  <ButtonLink url="/">Home</ButtonLink>
           <ModalLink modalContents={<MethodologyModalContents />}>Methodology</ModalLink>
-          <a href='/about'>About</a>
+          <ButtonLink url="/about">About</ButtonLink>
           <ModalLink modalContents={<LegalContents />}>Legal</ModalLink>
         </div>
         <div className='block sm:hidden'>
