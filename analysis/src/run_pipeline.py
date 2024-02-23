66a5634580a98a9a6f2966be29b19519c557da04
CanyonFoot <canyon.foot@twosigma.com>
2024-02-23 17:40:44 +0000

    New flags

--- analysis/src/run_pipeline.py
+++ analysis/src/run_pipeline.py
@@ -3,6 +3,7 @@ sys.path.append('../')
 from data.get_raw_data import get_raw_data
 from process.process_data import process_data
 from features.build_stop_features import get_stops_features
+from data.get_raw_data import get_national_data
 import os
 import json
 import argparse
@@ -31,7 +32,8 @@ def main():
     parser = argparse.ArgumentParser("Run data pipeline")
     parser.add_argument("--config", required=True)
     parser.add_argument("--city", default=False, required=False)
-    
+    parser.add_argument("--overwrite", action='store_true',required=False)
+    parser.add_argument("--get_national", action='store_true', required=False)
     opts = parser.parse_args()
     
     with open(opts.config) as f:
@@ -41,12 +43,19 @@ def main():
         msa_ids = str.split(opts.city,",")
     else: 
         msa_ids = config["MSA"]
+
+    if opts.get_national:
+        get_national_data(config)
         
     for msa_id in msa_ids:
-        print(f"Running Data pipeline for: {msa_id}")
-        # get_raw_data(config, msa_id)
-        # process_data(config, msa_id)
-        get_stops_features(config, msa_id, out=True)
+        output_path = f"{config['base_path']}/cities/{msa_id}/results/stop_features.geojson"
+        if os.path.exists(output_path) and opts.overwrite:
+            print(f"Running Data pipeline for: {msa_id}")
+            get_raw_data(config, msa_id)
+            process_data(config, msa_id)
+            get_stops_features(config, msa_id, out=True)
+        else:
+            print(f"Skipping {msa_id}: stop_features already exists")
     
     print(f"Combining the results")
     # concat_results(config, msa_ids)
